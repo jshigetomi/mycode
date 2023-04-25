04cb7d8723b182823c935f95a60827b824486efd
jshigetomi <justin.chung@tlgcohort.com>
2023-04-25 19:51:20 +0000

    join and split

---
+++ bustedcode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def calculator():
+   num1= input("Enter your first number: ")
+   num2= input("Enter your second number: ")
+   num3 = int(num1)+int(num2)
+   print(f"{num1} + {num2} = {num3}")
+
+calculator()
