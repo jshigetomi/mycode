40aef80b2abee08dfe81351a7b0c96b9de79c0ab
jshigetomi <justin.chung@tlgcohort.com>
2023-04-25 21:19:32 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+animalList = ["fox","fly","ant","bee","cod","cat","dog","yak","cow","hen","koi","hog","jay","kit"]
+
+for item in animalList:
+    print(item.title(), end=" ")
+print("\n")
